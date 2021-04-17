8f630934719ac4dde9052d3645b91d2c49257d2e
innabatalova <batalovainna97@gmail.com>
2021-04-17 18:30:50 +0300

    Create: added studies slider

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+const studiesSlider = new Swiper('.studies-slider', {
+  // Optional parameters
+  
+  loop: true,
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+
+  
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+ 
+});
+
+});
