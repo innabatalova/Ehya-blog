0c549b28029ec83eaa6aeec3be8c356e8ff0d833
innabatalova <batalovainna97@gmail.com>
2021-07-03 21:34:06 +0300

    Create: added button toogle formobile menu

--- js/main.js
+++ js/main.js
@@ -21,3 +21,18 @@ const studiesSlider = new Swiper('.studies-slider', {
 });
 
 });
+
+//переключатель для меню
+let menuButton = $(".navigation__mobile")
+
+menuButton.on("click", function(){
+  //  console.log ("Клик по кнопке меню")
+   $(".navbar").toggleClass("navbar__mobile") 
+   $(".select__link").toggleClass("navbar__link")
+   $(".icon-arrow").toggleClass("icon-arrow__mobile")
+   $(".container-select").toggleClass("container-select__mobile")
+   $(".navbar__link").toggleClass("navbar__link_mobile")
+   $(".navbar__linker").toggleClass("navbar__linker_mobile")
+   $(".navbar__item").toggleClass("navbar__item_mobile")
+
+});
